d2cb46e50b5e39f6edc59932c12de3af5833d5bb
Mike Wilcox <anm9tr@gmail.com>
2017-09-17 17:12:51 -0500

    sub components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import KeyPad from './KeyPad';
+import Display from './Display';
 import '../styles/Calculator.css';
 
 export default class Calculator extends Component {
@@ -32,30 +34,8 @@ export default class Calculator extends Component {
 	render () {
 		return (<div className="calculator">
 			<header>Calculator</header>
-			<section className="numbers">
-				<input value={this.state.text} onKeyUp={this.onText}/>
-			</section>
-			<section className="buttons" onClick={this.onButton}>
-				<button className="fn">C</button>
-				<button className="fn">BK</button>
-				<button className="fn">%</button>
-				<button className="op">÷</button>
-				<button>7</button>
-				<button>8</button>
-				<button>9</button>
-				<button className="op">x</button>
-				<button>4</button>
-				<button>5</button>
-				<button>6</button>
-				<button className="op">-</button>
-				<button>1</button>
-				<button>2</button>
-				<button>3</button>
-				<button className="op">+</button>
-				<button className="dbl">0</button>
-				<button>.</button>
-				<button className="op">=</button>
-			</section>
+			<Display onText={this.onText} value={this.state.text} />
+			<KeyPad onButton={this.onButton}/>
 		</div>);
 	}
 }
