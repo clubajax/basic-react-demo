13ae9c75095f48c2e0b6e2980fe5467f3e1bb441
Mike Wilcox <anm9tr@gmail.com>
2017-09-20 15:43:23 -0500

    first step

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,82 +1,49 @@
 import React, { Component } from 'react';
-import KeyPad from './KeyPad';
-import Display from './Display';
-import { getValue, getResult } from '../util/calc';
 import '../styles/Calculator.css';
 
 export default class Calculator extends Component {
 	constructor () {
 		super();
 
-		this.state = {
-			text: '',
-			mode: null,
-			expression: []
-		};
-
 		this.onButton = this.onButton.bind(this);
 		this.onText = this.onText.bind(this);
 	}
 
-	display (value) {
-		let exp;
-		const text = this.state.text;
-		switch (value.type) {
-			case 'digit':
-				if(this.state.mode === 'operand'){
-					this.setState({ text: value.value, mode: null });
-				} else {
-					this.setState({ text: text + value.value });
-				}
-				break;
-			case 'clear':
-				this.setState({ text: '', expression: [] });
-				break;
-			case 'backspace':
-				this.setState({ text: text.substring(0, text.length - 1) });
-				break;
-			case 'percentage':
-				this.setState({ text: Number(text) * 0.01 });
-				break;
-			case 'operand':
-				exp = this.state.expression;
-				if(!exp.length){
-					exp.push({ text });
-					exp.push({ operand: value.value });
-				} else {
-					exp[exp.length - 1].text = text;
-					exp.push({ operand: value.value });
-				}
-				this.setState({
-					expression: exp,
-					mode: 'operand'
-				});
-				break;
-			case 'equals':
-				exp = this.state.expression;
-				exp[exp.length - 1].text = text;
-				this.setState({ text: getResult(exp), expression: [], mode: 'operand' });
-				break;
-			default:
-				console.warn('unrecognized type', value.type);
-		}
-	}
-
 	onButton (e) {
-		this.display(getValue(e.target.textContent));
-
+		console.log('button', e.target.textContent);
 	}
 
 	onText (e) {
 		console.log('e.key', e.key);
-		this.display(getValue(e.key));
 	}
 
 	render () {
 		return (<div className="calculator">
 			<header>Calculator</header>
-			<Display onText={this.onText} value={this.state.text} />
-			<KeyPad onButton={this.onButton}/>
+			<section className="numbers">
+				<input onKeyUp={this.onText}/>
+			</section>
+			<section className="buttons" onClick={this.onButton}>
+				<button className="fn">C</button>
+				<button className="fn">BK</button>
+				<button className="fn">%</button>
+				<button className="op">÷</button>
+				<button>7</button>
+				<button>8</button>
+				<button>9</button>
+				<button className="op">x</button>
+				<button>4</button>
+				<button>5</button>
+				<button>6</button>
+				<button className="op">-</button>
+				<button>1</button>
+				<button>2</button>
+				<button>3</button>
+				<button className="op">+</button>
+				<button className="dbl">0</button>
+				<button>.</button>
+				<button className="op">=</button>
+			</section>
 		</div>);
 	}
 }
--- src/components/Display.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Display ({ onText, value }) {
-	return (
-		<section className="numbers">
-			<input value={value} onKeyUp={onText}/>
-		</section>
-	);
-}
